b6be73fdead2c9f5233b6e326a2c26516f97af8f
Bendick <bendickmh@gmail.com>
2022-04-11 11:49:12 +0200

    La inn funksjoner til main.cpp

--- Bachelor_StepperKontroll/src/main.cpp
+++ Bachelor_StepperKontroll/src/main.cpp
@@ -1,9 +1,78 @@
+/*
+* Dette programmet er skrevet av Erik Rognaldsen, Kim Sebastian Helmiksen og Bendick A O E R P Møller-Hansen
+* Det er skrevet til bruk av bachelor prosjekt, CNC - Fres og har ellers ingen funksjon.
+* Prosjektet er veiledet av Svein Haustveit og Frikk Fossdal
+*/
+
+// Inkluder nødvendige bibliotek
 #include <Arduino.h>
+#include <TMCStepper.h>
+
+// Deklarerer konstanter og pins
+
+// Deklarerer variabler
+
+// Setter opp motor drivere
+
+//  
+
 
 void setup() {
-  // put your setup code here, to run once:
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
+
+// Deklarerer Funksjoner for diverse formål
+
+// Funksjoner for posisjon setting og bevegelse av aksene
+void setXPos(int xPos) {
+
+}
+
+void setYPos(int yPos) {
+
+}
+
+void setZPos(int zPos) {
+
+}
+
+void movX(int xDest) {
+
+}
+
+void movY(int yDest) {
+
+}
+
+void movZ(int zDest) {
+
+}
+
+
+// Funksjoner for mottakelse og sending av seriell data
+void recieveData() {
+
+}
+
+void sendData() {
+
+}
+
+void confirmRecievedData() {
+
+}
+
+void confirmPos() {
+
+}
+
+void confirmNewPos(){
+
+}
+
+// Interrupts
+
