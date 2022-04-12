1de3210a451563dbe2f81d2aebe32d17cac9c8cd
BendickMH <bendickmh@gmail.com>
2022-04-12 10:00:35 +0200

    La til cpu_map.h og definerte noen variabler, har skrevet logg for 11.04.2022

--- Bachelor_StepperKontroll/src/main.cpp
+++ Bachelor_StepperKontroll/src/main.cpp
@@ -1,21 +1,14 @@
 /*
-* Dette programmet er skrevet av Erik Rognaldsen, Kim Sebastian Helmiksen og Bendick A O E R P Møller-Hansen
-* Det er skrevet til bruk av bachelor prosjekt, CNC - Fres og har ellers ingen funksjon.
-* Prosjektet er veiledet av Svein Haustveit og Frikk Fossdal
+Dette programmet er skrevet av Erik Rognaldsen, Kim Sebastian Helmiksen og Bendick A O E R P Møller-Hansen
+Det er skrevet til bruk av bachelor prosjekt, CNC - Fres og har ellers ingen funksjon.
+Prosjektet er veiledet av Svein Haustveit og Frikk Fossdal
+
+Dette arduino prosjektet er veldig inspirert av grbl, som er et open-source CNC program for arduino uno/mega. https://github.com/grbl/grbl
 */
 
 // Inkluder nødvendige bibliotek
 #include <Arduino.h>
 #include <TMCStepper.h>
-
-// Deklarerer konstanter og pins
-
-// Deklarerer variabler
-
-// Setter opp motor drivere
-
-//  
-
 
 void setup() {
   
@@ -24,55 +17,3 @@ void setup() {
 void loop() {
 
 }
-
-// Deklarerer Funksjoner for diverse formål
-
-// Funksjoner for posisjon setting og bevegelse av aksene
-void setXPos(int xPos) {
-
-}
-
-void setYPos(int yPos) {
-
-}
-
-void setZPos(int zPos) {
-
-}
-
-void movX(int xDest) {
-
-}
-
-void movY(int yDest) {
-
-}
-
-void movZ(int zDest) {
-
-}
-
-
-// Funksjoner for mottakelse og sending av seriell data
-void recieveData() {
-
-}
-
-void sendData() {
-
-}
-
-void confirmRecievedData() {
-
-}
-
-void confirmPos() {
-
-}
-
-void confirmNewPos(){
-
-}
-
-// Interrupts
-
